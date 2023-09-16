b05da47606d58c707a6e68c5f557be8b1799179f
Buch-dev <74716397+Buch-dev@users.noreply.github.com>
2023-09-16 10:42:31 +0100

    fifth commit

--- app/loading.jsx
+++ app/loading.jsx
@@ -5,6 +5,12 @@ export default function Loading() {
     <main className="text-center flex flex-col items-center justify-center h-screen">
       <h2>Loading...</h2>
       <p>Hopefully not for too long :)</p>
+      <div class="lds-ellipsis">
+        <div className="text-black"></div>
+        <div className="text-black"></div>
+        <div className="text-black"></div>
+        <div className="text-black"></div>
+      </div>
     </main>
   );
 }
